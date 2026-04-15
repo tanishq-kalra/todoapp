723fd3eedeba15857917f7abe0a561a8e0288744
tanishq-kalra <tanishq.k.kalra@gmail.com>
2026-04-15 19:31:36 +0530

    Fix Render CORS to allow Netlify frontend and localhost origins

--- backend/server.py
+++ backend/server.py
@@ -631,7 +631,11 @@ app.include_router(api_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "https://todokalra.netlify.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
